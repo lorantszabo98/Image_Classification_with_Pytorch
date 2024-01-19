206b4ed44e9b6165fc4178a94c0bf8479089e337
lorantszabo98 <64622519+lorantszabo98@users.noreply.github.com>
2024-01-19 10:22:11 +0100

    init commit

---
+++ evaluate.py
@@ -0,0 +1,24 @@
+import torch
+from created_models.simple_cnn_model import SimpleCNN
+from utils.data_loader import get_data_loaders
+
+
+def evaluate(model, test_loader):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    accuracy = 100 * correct / total
+    print('Accuracy of the network on the test images: {:.2f}%'.format(accuracy))
+
+if __name__ == "__main__":
+    model = SimpleCNN()
+    model.load_state_dict(torch.load('simple_cnn_model.pth'))
+    _, test_loader = get_data_loaders()
+    evaluate(model, test_loader)
--- train.py
+++ train.py
@@ -1,1 +1,33 @@
+import torch
+import torch.optim as optim
+from created_models.simple_cnn_model import SimpleCNN
+from utils.data_loader import get_data_loaders
+
+
+def train(model, train_loader, num_epochs=5):
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            inputs, labels = data
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            if i % 2000 == 1999:
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    print('Finished Training')
+
+if __name__ == "__main__":
+    model = SimpleCNN()
+    train_loader, _ = get_data_loaders()
+    train(model, train_loader)
+    torch.save(model.state_dict(), 'simple_cnn_model.pth')
 
