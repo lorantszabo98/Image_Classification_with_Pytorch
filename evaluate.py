e105b95eb0449e3a99f72ec5d7402fe5a0dcae21
lorantszabo98 <64622519+lorantszabo98@users.noreply.github.com>
2024-02-05 18:16:15 +0100

    display random pictures from the test dataset with their corresponding labels

--- evaluate.py
+++ evaluate.py
@@ -1,12 +1,73 @@
 import torch
 import os
 import torchvision.models as models
+import numpy as np
+import matplotlib.pyplot as plt
+import random
 from torch import nn
 from created_models.simple_cnn_model import SimpleCNN, SimpleCNN_v2, ImprovedCNN
 from utils.data_loader import get_data_loaders
 from utils.saving_loading_models import load_model
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from torch.utils.tensorboard import SummaryWriter
+
+
+def display_random_predictions(model, test_loader, class_labels, num_images=8, mode='default'):
+    # load the model based on the mode
+    if mode == "feature_extractor" or mode == "fine_tuning":
+        num_ftrs = model.fc.in_features
+        model.fc = nn.Linear(num_ftrs, 10)
+
+        load_model('./trained_models', model, mode=mode)
+    else:
+        load_model('./trained_models', model)
+    # set model to evaluation mode
+    model.eval()
+
+    # get the total number of images in the test set
+    total_images = len(test_loader.dataset)
+
+    # randomly select 6 indices from the test set
+    selected_indices = random.sample(range(total_images), num_images)
+
+    images_so_far = 0
+    # create a set of subplots in a grid, the first param is number of rows, the second param is the number of columns
+    fig, axs = plt.subplots(num_images // 2, 2, figsize=(10, 10))
+
+    with torch.no_grad():
+        for i, data in enumerate(test_loader):
+            images, labels = data
+            outputs = model(images)
+            _, preds = torch.max(outputs, 1)
+
+            # images is a tensor with shape (batch_size, channels, height, width)
+            # images.size()[0] is the number of images in the batch
+            for j in range(images.size()[0]):
+                # exit the loop when the desired number of images is reached
+                if images_so_far >= num_images:
+                    break
+
+                # Check if the current index is in the randomly selected indices
+                if i * test_loader.batch_size + j in selected_indices:
+                    #  accesses a specific subplot in the grid of subplots
+                    ax = axs[images_so_far // 2, images_so_far % 2]
+                    # turns of the axis
+                    ax.axis('off')
+                    #  sets the title of the subplot
+                    ax.set_title(f'predicted: {class_labels[preds[j]]}')
+
+                    # convert the image tensor to NumPy array and transpose
+                    img = np.transpose(images.cpu().data[j].numpy(), (1, 2, 0))
+                    # unnormalize if normalization was applied during data loading
+                    img = img * np.array([0.5, 0.5, 0.5]) + np.array([0.5, 0.5, 0.5])
+
+                    # show the image in the subplot
+                    ax.imshow(img)
+                    # counting the processed images
+                    images_so_far += 1
+
+    # after all show the plot
+    plt.show()
 
 
 def evaluate(model, test_loader, mode='default'):
@@ -56,6 +117,11 @@ if __name__ == "__main__":
     # model = ImprovedCNN()
     model = models.resnet18()
 
+    class_labels = [
+        'airplane', 'automobile', 'bird', 'cat', 'deer',
+        'dog', 'frog', 'horse', 'ship', 'truck'
+    ]
+
     # Create a SummaryWriter to use TensorBoard
     writer = SummaryWriter('logs')
     # Add the model graph to TensorBoard
@@ -66,4 +132,5 @@ if __name__ == "__main__":
     writer.close()
 
     _, test_loader = get_data_loaders(statistics=False)
-    evaluate(model, test_loader, mode="fine_tuning")
+    # evaluate(model, test_loader, mode="feature_extractor")
+    display_random_predictions(model, test_loader, class_labels)
